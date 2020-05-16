57b5038f854278418c468b454c2fa7166b96431a
Alecto <fomenkoandrey1978@gmail.com>
2020-05-16 22:57:18 +0300

    app update

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService, Task } from '../http.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hello',
@@ -7,9 +9,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  task: Task = {id: null, title: null, date: null};
+  tasks: Task[] = [];
+
+  constructor(private http: HttpService, private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
+    this.initDate();
+    this.buildForm();
+    this.readAllTasks();
+  }
+
+  private initDate() {
+    let date: Date | string = new Date();
+    let month: number | string = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    month = month < 10 ? `0${month}` : month;
+    date = `${year}-${month}-${day}`;
+
+    this.task.title = 'Привет мир!';
+    this.task.date = date;
+  }
+
+  private buildForm() {
+    this.form = this.fb.group({
+      title: [this.task.title, Validators.required],
+      date: [this.task.date, Validators.required]
+    });
+
+    this.form.valueChanges.subscribe(data => this.onValueChanges(data));
+  }
+
+  private onValueChanges(data?: any) {
+    if (!this.form) {
+      return;
+    }
+    const form = this.form;
+
+    console.clear();
+    console.log('TITLE: ', form.controls.title.value);
+    console.log(' DATE: ', form.controls.date.value);
+
+    this.task.title = form.controls.title.value;
+    this.task.date = form.controls.date.value;
+
+    console.log(this.task);
+  }
+
+  createTask() {
+    this.http.post(this.task).subscribe(res => this.tasks.push(res));
+  }
+
+  readAllTasks() {
+    this.http.get().subscribe(
+      res => {
+        Object.keys(res).forEach(key => {
+          const obj = Object.assign({}, res[key]);
+          obj.id = key;
+          this.tasks.push(obj);
+        });
+      });
+  }
+
+  updateTask(task: Task) {
+    task.title = this.task.title;
+    task.date = this.task.date;
+    this.http.update(task).subscribe(res => console.log(res));
+  }
+
+  deleteTask(task: Task) {
+    this.http.delete(task).subscribe(
+      () => this.tasks.splice(this.tasks.indexOf(task), 1)
+    );
   }
 
 }
+
---
+++ src/app/http.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+const url = 'https://angular-firebase-deploy-e3d15.firebaseio.com/posts';
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+export interface Task {
+  id?: string;
+  title: string;
+  date?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  post(task: Task): Observable<Task> {
+    return this.http.post<Task>(`${url}.json`, task, httpOptions).pipe(
+      map((res: any) => {
+        console.log('POST:', res);
+        return {
+          id: res.name,
+          title: task.title,
+          date: task.date
+        };
+      }),
+      catchError(this.errorHandler<Task>('POST'))
+    );
+  }
+
+  get(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${url}.json`, httpOptions).pipe(
+      tap(res => console.log('GET:', res)),
+      catchError(this.errorHandler<Task[]>('GET'))
+    );
+  }
+
+  update(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${url}/${task.id}.json`, task, httpOptions).pipe(
+      tap(res => console.log('PUT:', res)),
+      catchError(this.errorHandler<Task>('PUT'))
+    );
+  }
+
+  delete(task: Task): Observable<Task> {
+    return this.http.delete<Task>(`${url}/${task.id}.json`, httpOptions).pipe(
+      tap(res => console.log('DELETE:', res)),
+      catchError(this.errorHandler<Task>('DELETE'))
+    );
+  }
+
+  // Обрабатываем ошибку и продолжаем работу приложения.
+  private errorHandler<T>(operation: string, res?: T) {
+    return (err: any): Observable<T> => {
+      console.error(`${operation} failed: ${err}`);
+      return of(res as T);
+    };
+  }
+}
